680c88a49b71ab2ae9a96a2d35e6eb46d2f27d73
Arthur020104 <arthur020104@gmail.com>
2021-08-15 19:28:38 -0300

    adding first web project from week 8 (cs50)

---
+++ Web projects-cs50 week 8/lab8/script.js
@@ -0,0 +1,47 @@
+function checkRatio(event, id)
+{
+    let but = document.querySelector('#'+id);
+    let triggerObject = event.srcElement;
+    let reponse = triggerObject.innerHTML;
+    let answer = document.querySelector('#response_multi');
+    if(reponse == "a) eleitoral, que, ao instituir o voto direto para os cargos eletivos do Império, ao mesmo tempo em que proibiu o voto dos analfabetos, reduziu notavelmente a participação eleitoral dos setores populares.")
+    {
+        answer.innerHTML ="Your response is right!";
+        answer.style.visibility = 'visible';
+        answer.style.color= '#006400';
+        but.style.backgroundColor= '#004d26';
+        but.style.color= 'white';
+    }
+    else
+    {
+        answer.innerHTML ="Your response is wrong";
+        answer.style.visibility = 'visible';
+        answer.style.color= '#720000';
+        but.style.backgroundColor= '#720000';
+        but.style.color= 'white';
+    }
+}
+
+function submit()
+{
+    let response = document.querySelector('#response_free');
+    let answer = document.querySelector('#answer').value;
+    let estilo = document.querySelector('#answer');
+    if(answer.toLowerCase() == 'luis xiv')
+    {
+        estilo.style.backgroundColor= '#006400';
+        estilo.style.color= 'white';
+        response.innerHTML ="Your response is right!";
+        response.style.visibility = 'visible';
+        response.style.color= '#006400';
+        but.style.backgroundColor= '#004d26';
+    }
+    else
+    {
+        response.innerHTML ="Your response is wrong";
+        response.style.visibility = 'visible';
+        response.style.color= '#720000';
+        estilo.style.backgroundColor= '#720000';
+        estilo.style.color= 'white';
+    }
+}
