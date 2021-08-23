f74bddaa7f8921a786e0773da9c021205627b6bb
Arthur020104 <arthur020104@gmail.com>
2021-08-23 15:34:37 -0300

    adding problem set 8 tec js, html and css

--- Web projects-cs50 week 8/Problem 8/script.js
+++ Web projects-cs50 week 8/Problem 8/script.js
@@ -1,12 +1,13 @@
-function checkRatio(event, id)
+
+function checktf(event,id)
 {
     let but = document.querySelector('#'+id);
     let triggerObject = event.srcElement;//quando acontece tal evento (onclick) peg o elemento da fonte
     let reponse = triggerObject.innerHTML;
-    let answer = document.querySelector('#response_multi');
-    but.style.transition=".4s ease all"
-    answer.style.transition=".4s ease all"
-    if(reponse == "a) eleitoral, que, ao instituir o voto direto para os cargos eletivos do Império, ao mesmo tempo em que proibiu o voto dos analfabetos, reduziu notavelmente a participação eleitoral dos setores populares.")
+    let answer = document.querySelector("#response_multi");
+    but.style.transition=".4s ease all";
+    answer.style.transition=".4s ease all";
+    if(id == "buttrue")
     {
         answer.innerHTML ="Your response is right!";
         answer.className="alert alert-success";
@@ -23,27 +24,26 @@ function checkRatio(event, id)
         but.style.color= 'white';
     }
 }
-
 function submit()
 {
     let response = document.querySelector('#response_free');
     let answer = document.querySelector('#answer').value;
     let estilo = document.querySelector('#answer');
     estilo.style.transition=".5s ease all"
-    response.style.transition=".4s ease all"
-    if(answer.toLowerCase() == 'luis xiv')
+    response.style.transition=".4s ease all";
+    if(answer.toLowerCase() == 'ufu')
     {
-        estilo.style.backgroundColor= '#006400';
-        estilo.style.color= 'white';
+        estilo.style.backgroundColor= 'rgb(10, 85, 3)';
+        estilo.style.color= 'rgb(0, 255, 42)';
         response.innerHTML ="Your response is right!";
-        response.style.visibility = 'visible';
         response.className="alert alert-success";
+        response.style.visibility = 'visible';
     }
     else
     {
         response.innerHTML = "Your response is wrong";
-        response.style.visibility = 'visible';
         response.className="alert alert-danger";
+        response.style.visibility = 'visible';
         estilo.style.backgroundColor= '#720000';
         estilo.style.color= 'white';
     }
