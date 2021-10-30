b1a4932bf3feadd8114de59a4576743545e7c2b8
Arthur020104 <arthur020104@gmail.com>
2021-10-30 15:50:18 -0300

    organizing folders

--- problem/movies/11.sql
+++
@@ -1,2 +0,0 @@
-SELECT title FROM movies, ratings WHERE (id, movie_id) IN (SELECT movie_id,movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman"))
-ORDER BY(rating)DESC LIMIT 5;
--- problem/movies/7.sql
+++
@@ -1,1 +0,0 @@
-SELECT title, rating FROM movies, ratings WHERE year = 2010 AND movie_id = id AND rating IS NOT NULL ORDER BY(rating) DESC,title;
